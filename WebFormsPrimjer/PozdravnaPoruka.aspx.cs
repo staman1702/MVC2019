c521cb0bf56097c218f5d42c6d749817d6649d81
staman1702 <staman1702@gmail.com>
2019-03-27 18:05:15 +0100

    asp.net_webForm

---
+++ WebFormsPrimjer/PozdravnaPoruka.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsPrimjer
+{
+    public partial class PozdravnaPoruka : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnObradi_Click(object sender, EventArgs e)
+        {
+            LBLrezultat.Text = txtime.Text;
+
+        }
+
+        protected void txtime_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
