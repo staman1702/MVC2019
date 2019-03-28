dbccf1c0abacecd76e3dc6a6dd468d81920356a7
staman1702 <staman1702@gmail.com>
2019-03-28 18:49:15 +0100

    cetvrtak2803

---
+++ 2_1_vj/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2_1_vj.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string message = "Rezultat operacije 4 + 3 * 3 = ";
+            return View((object)message);
+        }
+
+        
+    }
+}
---
+++ HelloWorld/Controllers/PrviController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class PrviController : Controller
+    {
+        // GET: Prvi/MetodaSaParametrima/1212 na ovoj adresi cemo naci ga :)
+
+        public ActionResult MetodaSaParametrima(int id)
+        {
+
+            //return View();
+            return Content(id.ToString());
+        }
+
+        // GET: Prvi/DrugaMetodaSaParametrima/1212
+
+        public ViewResult DrugaMetodaSaParametrima(int id)
+        {
+
+            return View((object)id);
+            //return Content(id.ToString());
+        }
+    }
+}
