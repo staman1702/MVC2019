dd02ab0b62ff3403b5819b498a942760bc1f6bbd
staman1702 <staman1702@gmail.com>
2019-04-01 17:39:56 +0200

    zadaca

---
+++ BrojGodina/Controllers/BrojGodinaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrojGodina.Controllers
+{
+    public class BrojGodinaController : Controller
+    {
+        // GET: BrojGodina
+        public ViewResult RacunajBrojGodina()
+        {
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        public ViewResult RacunajBrojGodina(DateTime datum)
+        {
+            int brojGodina;
+            try
+            {
+                brojGodina = (DateTime.Now - datum).Days / 365;
+                return View((object)brojGodina.ToString());
+            }
+            catch (Exception e)
+            {
+                return View((object)e.Message);
+            }
+        }
+    }
+}
---
+++ BrojGodina/Controllers/TocanOdgovorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrojGodina.Controllers
+{
+    public class TocanOdgovorController : Controller
+    {
+        // GET: TocanOdgovor
+        // GET: BrojGodina
+        public ViewResult ProvjeriOdgovor()
+        {
+            return View();
+        }
+
+        //post
+        [HttpPost]
+        public ViewResult ProvjeriOdgovor(string odgovor)
+        {
+            string rezultat;
+            if (!string.IsNullOrEmpty(odgovor))
+            {
+                if (odgovor == "10")
+                {
+                    rezultat = "Odogovor je točan!";
+                    return View((object)rezultat);
+                }
+                else
+                {
+                    rezultat = "Odogovor nije točan!";
+                    return View((object)rezultat);
+                }
+            }
+            else
+            {
+                rezultat = "Odgovor ne postoji!";
+                return View((object)rezultat);
+            }
+
+        }
+
+    }
+}
