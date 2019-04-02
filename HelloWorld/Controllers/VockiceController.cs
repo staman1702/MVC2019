dee29ca0d6ca12671cc6dded4ce6d6f7e5680c16
staman1702 <staman1702@gmail.com>
2019-04-02 16:39:07 +0200

    upucajmeutorak

---
+++ HelloWorld/Controllers/VockiceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class VockiceController : Controller
+    {
+        // GET: Vockice
+        public ActionResult ZaForPetlju()
+        {
+            string[] voce = new string[] { "Jabuka", "Kruška", "Banana", "Limun" }; 
+            return View(voce);
+        }
+    }
+}
