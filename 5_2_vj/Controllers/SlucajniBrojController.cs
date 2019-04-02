189b39ba19ad4ebcf294ae63bb8f4b3ab35608e0
staman1702 <staman1702@gmail.com>
2019-04-02 14:33:58 +0200

    razor_sl_br

---
+++ 5_2_vj/Controllers/SlucajniBrojController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5_2_vj.Controllers
+{
+    public class SlucajniBrojController : Controller
+    {
+        // GET: SlucajniBroj
+        public ActionResult RazvrstajSlucajniBroj()
+        {
+            Random slucajniBroj = new Random();
+            int broj = slucajniBroj.Next(1, 1000);
+            return View(broj);
+        }
+    }
+}
