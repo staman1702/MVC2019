1acf54e473edb101bc94a0691f25df02cdba8d7b
staman1702 <staman1702@gmail.com>
2019-04-02 18:31:15 +0200

    krajutorka

---
+++ BrojGodina/Controllers/ParcijalniKontrolerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using BrojGodina.Models.Artikal;(ne treba aperentli)
+
+namespace BrojGodina.Controllers
+{
+    public class ParcijalniKontrolerController : Controller
+    {
+        // GET: ParcijalniKontroler
+        public ActionResult PrikaziKosaricu()
+        {
+            List<Artikal> ListaArtikala = new List<Artikal>()
+            {
+                new Artikal(){Cijena=9.99M, Kategorija="plava", Kolicina=5, Naziv="Keks"},
+                new Artikal(){Cijena=8.99M, Kategorija="zelena", Kolicina=7, Naziv="Kifla"},
+                new Artikal(){Cijena=5.99M, Kategorija="zuta", Kolicina=8, Naziv="Kroki"}
+            };
+            return View(ListaArtikala);
+        }
+    }
+}
