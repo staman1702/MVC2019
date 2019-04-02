ba6ed67d9ad668180609988195e6ef1d6f3cab87
staman1702 <staman1702@gmail.com>
2019-04-02 17:53:27 +0200

    utorakk

---
+++ 5_3_1_ponovno/Controllers/LayoutSectionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5_3_1.Controllers
+{
+    public class LayoutSectionController : Controller
+    {
+        // GET: LayoutSection
+        public ActionResult Stranica1()
+        {
+            return View();
+        }
+        public ActionResult Stranica2()
+        {
+            return View();
+        }
+    }
+}
---
+++ 5_3_1_ponovno/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_5_3_1_ponovno.MvcApplication" Language="C#" %>
