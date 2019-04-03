d985f7d13e39aa13dde5dfada9e72478bb0ab41e
staman1702 <staman1702@gmail.com>
2019-04-03 16:54:18 +0200

    juju view

---
+++ BrojGodina/Controllers/ChildActionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrojGodina.Controllers
+{
+    public class ChildActionController : Controller
+    {
+        // GET: ChildAction
+        public ViewResult ChildActionView()
+        {
+            string[] proizvodi = new string[] { "Banana", "Jabuka", "Kivi", "Mrkva", "Kupus", "Zec" };
+            return View(proizvodi);
+        }
+
+        //za sprijeciti direktno pozivanje/zloupotrebu kao obicna metoda ide ovaj annotation
+        [ChildActionOnly]
+        public string OdrediGrupuProizvoda(string proizvod)
+        {
+            switch (proizvod)
+            {
+                case "Banana":
+                case "Jabuka":
+                case "Kivi":
+                    return "voće";
+                case "Mrkva":
+                case "Kupus":
+                    return "povrće";
+                default:
+                    return "nepoznato";
+                
+            }
+        }
+    }
+}
