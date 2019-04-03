26726d17142609d5760bb4ec4d0914bfdab35ce8
staman1702 <staman1702@gmail.com>
2019-04-03 18:33:24 +0200

    kiša

---
+++ BrojGodina/Controllers/HtmlHelpersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrojGodina.Models;
+
+namespace BrojGodina.Controllers
+{
+    
+    public class HtmlHelpersController : Controller
+    {
+        private string[] mjesta = new string[] { "Split", "Osijek", "Rijeka", "Zagreb" };
+
+        // GET: HtmlHelpers
+        public ViewResult FormHelper()
+        {
+            ViewBag.Mjesta = this.mjesta;
+            return View(new Osoba());
+        }
+
+        [HttpPost]
+        public ViewResult FormHelper(Osoba osoba)
+        {
+            ViewBag.Mjesta = this.mjesta;
+            ViewBag.Poruka = "Podaci unešeni!";
+            return View(osoba);
+        }
+
+        // GET: HtmlHelpers
+        public ViewResult StrongTypeFormHelper()
+        {
+            ViewBag.Mjesta = this.mjesta;
+            return View(new Osoba());
+        }
+
+        [HttpPost]
+        public ViewResult StrongTypeFormHelper(Osoba osoba)
+        {
+            ViewBag.Mjesta = this.mjesta;
+            ViewBag.Poruka = "Podaci unešeni!";
+            return View(osoba);
+        }
+    }
+}
