be8ccdb3e0870613e2fb8407d17f3a29a3842044
staman1702 <staman1702@gmail.com>
2019-04-04 17:09:32 +0200

    templated helper

---
+++ BrojGodina/Controllers/TemplHtmlHelperiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrojGodina.Models;
+
+namespace BrojGodina.Controllers
+{
+    public class TemplHtmlHelperiController : Controller
+    {
+        // GET: TemplHtmlHelperi
+        public ActionResult HtmlEditorView()
+        {
+            return View(new OsobaTempl());
+        }
+        [HttpPost]
+        public ViewResult HtmlEditorView(OsobaTempl osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+        // GET: TemplHtmlHelperi
+        public ActionResult EditorModelView()
+        {
+            return View(new OsobaTempl());
+        }
+        [HttpPost]
+        public ViewResult EditorModelView(OsobaTempl osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+
+    }
+}
