7d37d770f04a08fb95d6a5a84705ca01d45368f4
staman1702 <staman1702@gmail.com>
2019-04-08 17:42:22 +0200

    baze_OPETTTTTT

---
+++ ADONET_spajanje_na_bazu/Controllers/SqlConnectionObjectControllerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ADONET_spajanje_na_bazu.Controllers
+{
+    public class SqlConnectionObjectControllerController : Controller
+    {
+        // GET: SqlConnectionObjectController
+        public ActionResult Index()
+        {
+            //prvo smo kreirali conn string 
+            string connString = 
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+
+            //zatim instanciramo taj conn
+            SqlConnection conn = new SqlConnection(connString);
+
+            //kada radimo konekcije sa bazom uvijek radimo try catch blok
+            try
+            {
+                conn.Open();
+                if(conn.State == ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela.");
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                Response.Write("greška spajanje na bazu" + sqlex.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write("neka greška" + ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+            return View();
+        }
+    }
+}
---
+++ ADONET_spajanje_na_bazu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ADONET_spajanje_na_bazu.MvcApplication" Language="C#" %>
