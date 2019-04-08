7b5d186494f3e4abb0056c67cf92d352c68c1d26
staman1702 <staman1702@gmail.com>
2019-04-08 18:03:36 +0200

    kuć!

--- ADONET_spajanje_na_bazu/Controllers/SqlConnectionObjectControllerController.cs
+++ ADONET_spajanje_na_bazu/Controllers/SqlConnectionObjectControllerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace ADONET_spajanje_na_bazu.Controllers
 {
     public class SqlConnectionObjectControllerController : Controller
     {
-        // GET: SqlConnectionObjectController
+        // GET: SqlConnectionObjectController direktno
         public ActionResult Index()
         {
             //prvo smo kreirali conn string 
@@ -26,6 +27,43 @@ namespace ADONET_spajanje_na_bazu.Controllers
             {
                 conn.Open();
                 if(conn.State == ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela.");
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                Response.Write("greška spajanje na bazu" + sqlex.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write("neka greška" + ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+            return View();
+        }
+
+
+        // GET: SqlConnectionObjectController preko Web.config
+        public ActionResult PrekoWebConfig()
+        {
+
+            string connString = ConfigurationManager.ConnectionStrings["dbAlgebraConnString"].ConnectionString;
+
+            
+            SqlConnection conn = new SqlConnection(connString);
+
+            
+            try
+            {
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
                 {
                     Response.Write("Konekcija je uspjela.");
                 }
