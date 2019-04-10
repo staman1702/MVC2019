89f584b8b76434aafaf972fb3432f3faf4e0d2f9
staman1702 <staman1702@gmail.com>
2019-04-10 17:35:30 +0200

    srijeda

--- ADO_NET_Komponente/Controllers/ADONETKomponenteController.cs
+++ ADO_NET_Komponente/Controllers/ADONETKomponenteController.cs
@@ -14,18 +14,19 @@ namespace ADO_NET_Komponente.Controllers
     {
         //prvo smo kreirali conn string 
         string ConnStr =
-            @"Data Source=MISTY\SQLEXPRESS;Initial Catalog = dbAlgebra2; Integrated Security = True";
-
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
         public ActionResult List()
         {
             //instanciranje konekcije
             SqlConnection conn = new SqlConnection(ConnStr);
                         
-            string cmdTxt = "";
-            cmdTxt += "SELECT count(*) FROM [dbo].[tblTecajevi] ";
+            //string cmdTxt = "";
+            //cmdTxt += "SELECT * FROM tblPolaznici";
 
-            SqlCommand cmd = new SqlCommand(cmdTxt, conn);
-            cmd.Connection.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT * FROM tblPolaznici";
 
             SqlDataReader dr = null;
             List<Polaznik> lstPolaznici = new List<Polaznik>();
@@ -46,6 +47,7 @@ namespace ADO_NET_Komponente.Controllers
                             polaznik.Ime = dr["Ime"].ToString();
                             polaznik.Prezime = dr["Prezime"].ToString();
                             polaznik.Email = dr["Email"].ToString();
+                            lstPolaznici.Add(polaznik);
                         }
                     }
                 }
@@ -74,6 +76,107 @@ namespace ADO_NET_Komponente.Controllers
             return View(lstPolaznici);
         }
 
-        
+        [HttpGet]
+        public ActionResult Details(int idPolaznik)
+        {
+            SqlConnection conn = new SqlConnection(ConnStr);
+            string cmdText = "SELECT * FROM tblPolaznici WHERE"
+                +" IdPolaznik=@IdPolaznik";
+
+            SqlCommand cmd = new SqlCommand(cmdText, conn);
+
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = "@IdPolaznik";
+            param.DbType = DbType.Int32;
+            param.Direction = ParameterDirection.Input;
+            param.Value = idPolaznik;
+            cmd.Parameters.Add(param);
+
+            SqlDataReader dr = null;
+            Polaznik polaznik = new Polaznik();
+
+            try
+            {
+                conn.Open();
+                dr = cmd.ExecuteReader();
+                if (dr != null)
+                {
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {                       
+                            polaznik.IdPolaznik = int.Parse(dr["IdPolaznik"].ToString());
+                            polaznik.Ime = dr["Ime"].ToString();
+                            polaznik.Prezime = dr["Prezime"].ToString();
+                            polaznik.Email = dr["Email"].ToString();                           
+                        }
+                    }
+                }
+
+            }
+            catch (SqlException sqlex)
+            {
+                ViewBag.Message = "SQL greška:" + sqlex.Message;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Greška:" + ex.Message;
+            }
+            finally
+            {
+                if (dr != null)
+                {
+                    dr.Close();
+                }
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return View(polaznik);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Polaznik());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Polaznik model)
+        {
+            string ConnStr =
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dbAlgebra;Integrated Security=True";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnStr))
+                {
+                    string cmdText = "";
+                    cmdText += "INSERT INTO tblPolaznici ";
+                    cmdText += "(Ime, Prezime, Email) ";
+                    cmdText += "VALUES ";
+                    cmdText += "('" + model.Ime + "', '"
+                        + model.Prezime + "', '" + model.Email + "') ";
+
+                    SqlCommand cmd = new SqlCommand(cmdText, conn);
+                    cmd.Connection.Open();
+
+                    int brojDodanihRedaka = cmd.ExecuteNonQuery();
+                    ViewBag.Message = "Broj dodanih redaka: " + brojDodanihRedaka;
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                ViewBag.Message = "SQL greška:" + sqlex.Message;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Greška:" + ex.Message;
+            }
+
+            return View(model);
+
+        }
+
     }
 }
