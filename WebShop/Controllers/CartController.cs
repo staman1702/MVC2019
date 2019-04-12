8c17eeb6f9d66e47c5254abafc18c109df61c767
staman1702 <staman1702@gmail.com>
2019-04-12 18:17:13 +0200

    cart is done

---
+++ WebShop/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class CartController : Controller
+    {
+        //konekcija na bazu
+        private WebShopEntities db = new WebShopEntities();
+
+        public static List<Proizvodi> lstProizvodi = new List<Proizvodi>();
+
+        // GET: Cart
+        public ActionResult Index()
+        {
+            if (Session["Cart"]!=null)
+            {
+                lstProizvodi = Session["Cart"] as List<Proizvodi>;
+            }
+            return View(lstProizvodi);
+        }
+
+
+        public ActionResult AddToCart(int id)
+        {
+            Proizvodi proizvod = db.Proizvodis.Find(id);
+            lstProizvodi.Add(proizvod);
+
+            Session["Cart"] = lstProizvodi;
+
+            if(proizvod==null)
+            {
+                return HttpNotFound();
+            }
+
+            var proizvodi = db.Proizvodis.Include(p => p.MjereProizvoda);
+
+            return RedirectToAction(actionName: "Index", controllerName: "WebShop", routeValues: proizvodi.ToList());
+        }
+
+
+        //remove
+        public ActionResult RemoveFromCart(int index)
+        {
+            lstProizvodi = Session["Cart"] as List<Proizvodi>;
+            lstProizvodi.RemoveAt(index);
+            Session["Cart"] = lstProizvodi;
+            
+            return View("Index",lstProizvodi);
+        }
+    }
+}
---
+++ WebShop/Controllers/WebShopController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using WebShop.Models;
+
+
+namespace WebShop.Controllers
+{
+    public class WebShopController : Controller
+    {
+        //konekcija
+        private WebShopEntities db = new WebShopEntities();
+
+        // GET: WebShop
+        public ActionResult Index()
+        {
+            var proizvodi = db.Proizvodis.Include(p => p.MjereProizvoda);
+            return View(proizvodi.ToList());
+        }
+    }
+}
