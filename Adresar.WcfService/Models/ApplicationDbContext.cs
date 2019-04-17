07a10c6ae1c4a08603a23d3195a277f53262db3a
staman1702 <staman1702@gmail.com>
2019-04-17 16:58:10 +0200

    wcf1

---
+++ Adresar.WcfService/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Adresar.WcfService.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("AdresarBaza")
+        {
+        }       
+
+        public virtual DbSet<Kontakt> Kontakti { get; set; }
+    }
+    {
+    }
+}
---
+++ Adresar.WcfService/Models/Kontakt.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Adresar.WcfService.Models
+{
+    [DataContract]
+
+    public class Kontakt
+    {
+        [DataMember]  //za svako svojstvo koje zelim da moze mi se dohvatiti sa druge strane
+        [Key]
+        public int Id { get; set; }
+
+        [DataMember]
+        [Required]
+        [StringLength(25, ErrorMessage ="Ime ne smije biti veće od 25 znakova")]
+        public string Ime { get; set; }
+
+        [DataMember]
+        [Required]
+        [StringLength(25)]
+        public string Prezime { get; set; }
+
+        [DataMember]
+        [StringLength(100)]
+        public string Adresa { get; set; }
+
+        [DataMember]
+        [StringLength(20)]
+        public string Telefon { get; set; }
+
+        [DataMember]
+        [StringLength(20)]
+        public string Mobitel { get; set; }
+
+        [DataMember]
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Napomena { get; set; }
+
+        [DataMember]
+        public bool Aktivan { get; set; }
+
+    }
+}
