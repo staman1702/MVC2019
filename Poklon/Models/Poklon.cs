83d300ba4ac9cfd0e0528c2600f1e1cb94bbaefb
staman1702 <staman1702@gmail.com>
2019-04-18 16:24:25 +0200

    1212

---
+++ Poklon/Models/Poklon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Poklon.Models
+{
+    public class Poklon
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [StringLength(25, ErrorMessage ="Naziv ne smije biti duži od 25")]
+        public string Naziv { get; set; }
+
+        public float Cijena { get; set; }
+
+        public bool Naruceno { get; set; }
+
+
+    }
+}
